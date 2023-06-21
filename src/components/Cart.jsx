469ef227ae5d66ddcb5d21a430a13100232ebf50
Harish_S_99 <s.harish2711@gmail.com>
2023-06-21 13:30:54 +0530

    need to do total sum

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -13,7 +13,6 @@ function Cart() {
   }, []);
 
   const removeFromCartHandler = (event) => {
-    // console.log(event.target.value);
     var newArr = selectedId;
     var neededDeletionIndex = newArr.indexOf("2");
     newArr.splice(neededDeletionIndex, 1);
@@ -22,44 +21,69 @@ function Cart() {
   };
 
   return (
-    <div className="cart-content">
-      {selectedId?.map((itemInSelectedId, ind) => {
-        return fetchedProductsInCart?.map((itemInFetchedProductsInCart) => {
-          if (itemInFetchedProductsInCart.id == itemInSelectedId) {
-            console.log(itemInFetchedProductsInCart.id);
-            return (
-              <div className="flip-card" key={ind}>
-                <div className="flip-card-inner">
-                  <div className="flip-card-front">
-                    <img
-                      src={itemInFetchedProductsInCart.image}
-                      alt={itemInFetchedProductsInCart.name}
-                      className="products-image"
-                    />
+    <>
+      <div className="cart-content">
+        {selectedId.length > 0 ? (
+          <>
+            <h1>Items In Your Cart : </h1>
+            <br></br>
+          </>
+        ) : (
+          <h2>
+            Please add Items to your cart! <br></br>
+          </h2>
+        )}
+        <div className="products-content">
+          {selectedId?.map((itemInSelectedId, ind) => {
+            return fetchedProductsInCart?.map((itemInFetchedProductsInCart) => {
+              if (itemInFetchedProductsInCart.id == itemInSelectedId) {
+                console.log(itemInFetchedProductsInCart.id);
+                return (
+                  <div className="flip-card" key={ind}>
+                    <div className="flip-card-inner">
+                      <div className="flip-card-front">
+                        <img
+                          src={itemInFetchedProductsInCart.image}
+                          alt={itemInFetchedProductsInCart.name}
+                          className="products-image"
+                        />
+                      </div>
+                      <div className="flip-card-back">
+                        <h1 className="products-name">
+                          {itemInFetchedProductsInCart.name}
+                        </h1>
+                        <b className="products-cost">
+                          {itemInFetchedProductsInCart.cost}
+                        </b>
+                        {/* <p className="products-desc">{eachItem.description}</p> */}
+                        <button
+                          className="products-btn"
+                          onClick={removeFromCartHandler}
+                          value={itemInSelectedId}
+                        >
+                          Remove From Cart
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                  <div className="flip-card-back">
-                    <h1 className="products-name">
-                      {itemInFetchedProductsInCart.name}
-                    </h1>
-                    <b className="products-cost">
-                      {itemInFetchedProductsInCart.cost}
-                    </b>
-                    {/* <p className="products-desc">{eachItem.description}</p> */}
-                    <button
-                      className="products-btn"
-                      onClick={removeFromCartHandler}
-                      value={itemInSelectedId}
-                    >
-                      Remove From Cart
-                    </button>
-                  </div>
-                </div>
-              </div>
-            );
-          }
-        });
-      })}
-    </div>
+                );
+              }
+            });
+          })}
+        </div>
+        <div>
+          {selectedId.length > 0 ? (
+            <>
+              <h1>Proceed to Checkout : </h1>
+              <br></br>
+              <h3>Total Sum : {}</h3>
+            </>
+          ) : (
+            <br></br>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
